e25cb75618e75241b2ad24c3fc6e60a360eef6da
Eryk Miszczuk <eryk.miszczuk1996@gmail.com>
2019-03-21 10:07:04 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const path = require('path');
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+
+app.get('/home', (req, res) => {
+  res.send({
+    data: 'hello world'
+  });
+});
+
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
+
